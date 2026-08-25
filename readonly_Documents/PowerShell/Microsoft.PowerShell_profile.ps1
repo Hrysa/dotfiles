1e3e6aeb8457d4ee5c81c2a41d1118f1e95a3a40
Val Hsu <vesper.val@qq.com>
2026-08-25 17:19:25 +0800

    update pwsh profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,20 @@
 $env:POSH_GIT_ENABLED = $false
 
-function Ensure-Module {
-    param([string]$ModuleName)
-    if (-not (Get-Module -ListAvailable $ModuleName)) {
-        Write-Host "installing module: $ModuleName" -ForegroundColor Yellow
-        Install-Module -Name $ModuleName -Scope CurrentUser -AllowClobber -Force
-    }
-}
-
-Ensure-Module -ModuleName Z
-Ensure-Module -ModuleName posh-git
-Ensure-Module -ModuleName CompletionPredictor
-
 function prompt {
-  if ("$(Get-Location)" -Eq $HOME) {
-    $p = "~"
-  } else {
-    $p = Split-Path -leaf -path (Get-Location)
+  if ("$(Get-Location)" -Ne $HOME) {
     $gitBranch = git rev-parse --abbrev-ref HEAD 2>$null
+  }
+
+  $dir = [System.IO.DirectoryInfo](Get-Location).Path
+
+  if (-not $dir.Parent) {
+    $p = $dir.FullName
+  }
+  elseif (-not $dir.Parent.Parent) {
+    $p = $dir.Name
+  }
+  else {
+    $p = "$($dir.Parent.Name)\$($dir.Name)"
   }
 
   $dt = [System.DateTime]::Now.ToString("HH:mm.ss");
@@ -37,34 +34,33 @@ function gcmt { git commit -m "$args" }
 function ssh { ssh.exe $args; $Host.UI.RawUI.WindowTitle = "PowerShell" }
 function kn { k -n (Split-Path -leaf -path (Get-Location)) $args }
 
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-
-# Only enable prediction features when VT processing is available (interactive terminal)
-if (-not [Console]::IsOutputRedirected) {
-    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-    Set-PSReadLineOption -PredictionViewStyle ListView
-}
-
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-# Exit with ctrl-d
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-
-# Autocompletion for arrow keys
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 $null = Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount 1 -Action {
-  Import-Module Z
-  Import-Module posh-git
-  Import-Module CompletionPredictor
+  # Only enable prediction features when VT processing is available (interactive terminal)
+  if (-not [Console]::IsOutputRedirected) {
+      Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+      Set-PSReadLineOption -PredictionViewStyle ListView
+  }
+
+  # Shows navigable menu of all options when hitting Tab
+  Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+  # Exit with ctrl-d
+  Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+  # Autocompletion for arrow keys
+  Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
   If (Get-Command -Name "kubectl" -ErrorAction SilentlyContinue) {
     Invoke-Expression "$(kubectl completion powershell)"
     Register-ArgumentCompleter -CommandName 'k' -ScriptBlock $__kubectlCompleterBlock
     Register-ArgumentCompleter -CommandName 'kn' -ScriptBlock $__kubectlCompleterBlock
   }
+
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
 
 Set-Alias k kubectl
